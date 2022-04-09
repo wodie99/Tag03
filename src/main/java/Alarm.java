7c00a9a6f9f72bccb80a352768e3ea592e187bb9
wolfgang099 <git@wodie.org>
2022-04-09 18:26:12 +0200

    Message for end of Program

--- src/main/java/Alarm.java
+++ src/main/java/Alarm.java
@@ -3,6 +3,7 @@ public class Alarm {
         int anzahlPersonen = 10;
         String alarmstufe = "gelb";
         System.out.println(zuVielePersonen(anzahlPersonen,alarmstufe));
+        System.out.println("Programmende");
 
     }
 
